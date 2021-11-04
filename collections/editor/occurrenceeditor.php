03b7b1e969e46f3d4484f388e1c57f9c302e3c61
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-11-04 07:58:58 -0700

    Still attempt to toggle georefExtraDiv

--- collections/editor/occurrenceeditor.php
+++ collections/editor/occurrenceeditor.php
@@ -1132,7 +1132,7 @@ else{
 												$georefExtraDiv .= "block";
 											}
 											?>
-											<div id="georefExtraDiv" style="<?php echo $georefExtraDiv; ?>;">
+											<div id="georefExtraDiv" > <!-- style="*<?php // echo $georefExtraDiv; ?>;" -->
 												<div> <!--style="clear:both;"-->
 													<div id="georeferencedByDiv">
 														<?php echo (defined('GEOREFERENCEDBYLABEL')?GEOREFERENCEDBYLABEL:'Georeferenced By'); ?>
