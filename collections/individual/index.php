b0b451ac569c428a5a75e8a4069fbc5d783bc95e
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-11-05 09:09:50 -0700

    underline contact info

--- collections/individual/index.php
+++ collections/individual/index.php
@@ -1033,7 +1033,7 @@ $traitArr = $indManager->getTraitArr();
 									echo (isset($LANG['SEEERROR'])?$LANG['SEEERROR']:'Do you see an error? If so, errors can be fixed using the').' ';
 									?>
 									<a href="../editor/occurrenceeditor.php?occid=<?php echo $occArr['occid'];?>">
-										<?php echo (isset($LANG['OCCEDITOR'])?$LANG['OCCEDITOR']:'Occurrence Editor'); ?>.
+										<?php echo '<u>'.(isset($LANG['OCCEDITOR'])?$LANG['OCCEDITOR']:'Occurrence Editor').'</u>'; ?>.
 									</a>
 									<?php
 								}
